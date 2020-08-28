332d2f170e4435acc2ad23df07981af6261e1a70
Vlad <VladDaImpaler@gmail.com>
2020-08-28 03:21:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,9 @@
 @cool = @post.categories.build(name: "Cool")
 @post.save
 
-@category = Category.create(name: "Cool")
 @post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
 @user = User.create(username: 'RealBigFish')
-@cool = Category.create(name: 'Cool')
+Category.create(name: 'Lame')
 
 @user = User.create(username: 'CoolGuy5')
 @comment = @post.comments.build(content: "Great Post!", user: @user)
